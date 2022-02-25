cf3983a106a79cbb487ef4789d444ab4962a48c4
ItsHMS <hamidmehmood816@gmail.com>
2022-02-25 15:26:38 +0500

    Hamid

--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
-document.getElementById("demo").innerHTML = 5 + 6;
+$(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+   
+        navigation : true, // Show next and prev buttons
+   
+        // slideSpeed : 2500,
+        // paginationSpeed : 2500,
+        loop:true,
+        items : 1, 
+        autoplay:true,
+        nav:false,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+        
+
+        
+   
+    });
+
+   
+  });
+
+$(window).scroll(function(){
+  var top=$(window).scrollTop();
+  if(top>=112){
+    $("header").addClass('secondary-dark-blue-bg');
+  }
+  else{
+    if($("header").hasClass('secondary-dark-blue-bg')){
+      $("header").removeClass('secondary-dark-blue-bg');
+    }
+    
+
+  }
+});
+
+  /*
+  
+  var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+  
+  */
+
